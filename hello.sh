2ca36941c41153678c85d661c3bc0c61d2feca42
shashankpanchwatkar <shashankpanchwatkar12345@gmail.com>
2026-08-18 04:27:39 +0000

    freefire is the only game added

---
+++ hello.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+
+echo "Hello, $USER"
+echo "Welcome to Linux"
+echo "Today I am learning DevOps"
--- input.sh
+++ input.sh
@@ -2,6 +2,6 @@
 
 echo "What is your name?"
 read name
-
+echo "free fire is the only game"
 echo "Hello $name"
 echo "this is added in the file "
